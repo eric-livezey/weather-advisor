9f93233794202f6d5702b3bbc5e6b32d8f6d57ef
eric-livezey <eric.livezey@gmail.com>
2025-03-07 00:47:19 -0500

    Update accuweather

--- api/accuweather/index.js
+++ api/accuweather/index.js
@@ -2,40 +2,6 @@
  * For scraping weather directly from accuweather
  */
 
-// const BASE_URL = "https://weather.com/api/v1/p";
-
-// async function request(endpoint, payload) {
-//     const url = new URL(BASE_URL + endpoint);
-//     const headers = {
-//         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36",
-//         "Origin": "https://weather.com",
-//         "Referrer": "https://weather.com/",
-//         "Content-Type": "application/json"
-//     };
-//     const res = await fetch(url, { method: "POST", body: Buffer.from(JSON.stringify(payload)), headers });
-//     if (res.ok)
-//         return await res.json();
-//     else
-//         throw new Error(`${res.status} ${res.statusText}`);
-// }
-
-// async function getLocation(query) {
-//     const payload = [
-//         {
-//             name: "getSunV3LocationSearchUrlConfig",
-//             params: {
-//                 query,
-//                 language: "en-US",
-//                 locationType: "locale"
-//             }
-//         }
-//     ]
-//     const res = await request("/redux-dal", payload);
-//     const config = payload[0];
-//     const key = Object.entries(config.params).sort(([k1], [k2]) => k1.localeCompare(k2)).map(([k, v]) => `${k}:${v}`).join(";");
-//     return res.dal[config.name][key];
-// }
-
 const ITEM = /<a [^>]*class="[^"]*hourly-list__list__item[^"]*"[^>]*>((?:(?!<\/a>).)*)<\/a>/g;
 const TIME = /<span [^>]*class="[^"]*hourly-list__list__item-time[^"]*"[^>]*>((?:(?!<\/span>).)*)<\/span>/g;
 const TEMPERATURE = /<span [^>]*class="[^"]*hourly-list__list__item-temp[^"]*"[^>]*>((?:(?!<\/span>).)*)<\/span>/g;
