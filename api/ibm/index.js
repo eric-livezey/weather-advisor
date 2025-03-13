2f9ed8d5e344c1a19f24773bfb857d30c3248147
eric-livezey <eric.livezey@gmail.com>
2025-03-13 17:09:04 -0400

    Minor change

--- api/ibm/index.js
+++ api/ibm/index.js
@@ -2,32 +2,6 @@ import { request } from "../utils.js";
 
 const BASE_URL = "https://api.weather.com";
 
-// async function request(endpoint, params) {
-//     const url = new URL(BASE_URL + endpoint);
-//     for (const [key, value] of Object.entries(params))
-//         url.searchParams.append(key, value);
-//     url.searchParams.set("apiKey", process.env.WEATHER_COMPANY_API_KEY);
-//     const res = await fetch(url);
-//     if (res.ok)
-//         return await res.json();
-//     else
-//         throw new Error(`${res.status} ${res.statusText}`);
-// }
-
-// async function getForecastHourly2Day(lat, lng) {
-//     const params = {
-//         geocode: `${lat},${lng}`,
-//         format: "json",
-//         units: "e",
-//         language: "en-US"
-//     }
-//     return await request("/forecast/hourly/2day", params);
-// }
-
-// export {
-//     getForecastHourly2Day
-// };
-
 /**
  * @param {string} endpoint 
  * @param {import("../utils").RequestOptions} options 
